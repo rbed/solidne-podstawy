992e0ad9249f2542dd931ff059e9667d74a5fa4f
R <radekbednarz@gmail.com>
2020-06-25 20:10:45 +0200

    build + other changes

--- server/app.js
+++ server/app.js
@@ -16,12 +16,14 @@ var app = express();
 logger.pringAppLogo();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+// app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "client/build"));
 app.set("view engine", "jade");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build/index.html")));
 
 
 /**
